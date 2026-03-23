f616bf7753a0c925fb62afdbaf8491dbf22fba38
daydreamboy <wesley4chen@gmail.com>
2026-03-23 10:58:54 +0800

    use WebP-Xcode xcode project to build WebP.framework instead of use scripts

---
+++ WebP-Xcode/WebP/WebP.h
@@ -0,0 +1,25 @@
+//
+//  WebP.h
+//  WebP
+//
+//  Created by wesley_chen on 2026/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebP.
+FOUNDATION_EXPORT double WebPVersionNumber;
+
+//! Project version string for WebP.
+FOUNDATION_EXPORT const unsigned char WebPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebP/PublicHeader.h>
+
+#import <WebP/decode.h>
+#import <WebP/demux.h>
+#import <WebP/encode.h>
+#import <WebP/format_constants.h>
+#import <WebP/mux.h>
+#import <WebP/mux_types.h>
+#import <WebP/types.h>
+#import <WebP/sharpyuv.h>
